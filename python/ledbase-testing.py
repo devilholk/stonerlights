cc5818c53683855014204299bf7abec8bc806168
Devilholk <devilholk@kabelstegeslampa>
2013-03-27 22:17:03 +0100

    Committing code forgotten on the kabelstegeslampa

--- python/ledbase-testing.py
+++ python/ledbase-testing.py
@@ -15,20 +15,43 @@ class SerialLight(LED_interface):
 	def __init__(self, port, baudrate=460800):
 		self.port = serial.Serial(port, baudrate)
 	
-	def update(self, led):
+	def update(self, led, r, g, b):
 		top = (1<<led.bits)-1
-		i16 = lambda n: chr((n>>8)&0xFF)+chr(n&0xFF)
+		i16 = lambda n: chr(n&0xFF)+chr((n>>8)&0xFF)
 		i = lambda n: i16(int(top*n))
-		data = chr(led.address & 0xFF) + i(led.r) + i(led.g) + i(led.b)
+		data = chr(led.address & 0xFF) + i(r) + i(g) + i(b)
 		self.port.write(data)
 		
 lilla_lampan = Pointlight(1)
 ljusslinga = Pointlight(2)
 
-crf_conference = SerialLight('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AE009C7E-if00-port0', 115200)
+crf_conference = SerialLight('/dev/serial/by-path/pci-0000:00:10.3-usb-0:1:1.0-port0', 115200)
 crf_conference.attach(lilla_lampan)
 crf_conference.attach(ljusslinga)
 
+ljusslinga.rcomp, ljusslinga.gcomp, ljusslinga.bcomp = 1.0, 0.8, 0.65
 
-ljusslinga.Preset('orange')
-ljusslinga.update()
+import time
+
+def sintest(leds, w):
+	v=0.0
+	q=0.0
+	while(1):
+		v+=0.01
+		for l in leds:
+			q+=0.1
+			l.r=math.sin(q*0.2+v)*0.5+0.5
+			l.g=math.sin(q*0.34+v*1.23)*0.5+0.5
+			l.b=math.sin(q*-.3+v*1.84)*0.5+0.5
+			l.update()
+
+		time.sleep(w)
+
+def test(l, r,g,b):
+	global rcomp,gcomp,bcomp
+	l.r=math.pow(r*rcomp,2.5)
+	l.g=math.pow(g*gcomp,2.5)
+	l.b=math.pow(b*bcomp,2.5)
+	l.update()
+
+sintest( (ljusslinga, lilla_lampan), 0.01 )
--- python/ledbase.py
+++ python/ledbase.py
@@ -1,33 +1,50 @@
 #encoding=utf-8
-
+import math
 R, G, B = 1, 2, 4
 
 Presets = {
-	'white':		(1.0, 1.0, 1.0),
-	'black':		(0.0, 0.0, 0.0),
+	'white':	(1.0, 1.0, 1.0),
+	'black':	(0.0, 0.0, 0.0),
 	'red':		(1.0, 0.0, 0.0),
 	'orange':	(1.0, 0.5, 0.0),
 	'yellow':	(1.0, 1.0, 0.0),
-	'green':		(0.0, 1.0, 0.0),
+	'green':	(0.0, 1.0, 0.0),
 	'cyan':		(0.0, 1.0, 1.0),
 	'blue':		(0.0, 0.0, 1.0),
 	'magenta':	(1.0, 0.0, 1.0),
-	'white':		(1.0, 1.0, 1.0)
+	'white':	(1.0, 1.0, 1.0)
 }
 
 class LED():
 	channels = R | G | B
 	bits = 1
 	r, g, b = 0.0, 0.0, 0.0
+	rcomp, gcomp, bcomp = 1.0, 1.0, 1.0
+	e = 2.5
 	controller = None
 	
 	def update(self):
 		if self.controller != None:
-			self.controller.update(self)
+			
+			r = self.r * self.rcomp
+			g=self.g * self.gcomp
+			b=self.b * self.bcomp
+			if r > 1.0: r = 1.0
+			if r < 0.0: r = 0.0
+			if g > 1.0: g = 1.0
+			if g < 0.0: g = 0.0
+			if b > 1.0: b = 1.0
+			if b < 0.0: b = 0.0
+			r=math.pow(r, self.e)
+			g=math.pow(g, self.e)
+			b=math.pow(b, self.e)
 
-	def Preset(self, name):
+			self.controller.update(self, r, g, b)
+
+	def Preset(self, name, update=True):
 		self.r, self.g, self.b = Presets[name]
-
+		if update: 
+			self.update()
 	
 
 class LED_array(LED):
